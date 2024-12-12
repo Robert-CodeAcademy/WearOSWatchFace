eb0b8cf0d7960c20581a4197c1b63f1e7deddd9c
Robert-CodeAcademy <108361159+Robert-CodeAcademy@users.noreply.github.com>
2024-12-12 17:55:30 +0100

    Commit 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,28 +1,28 @@
 plugins {
-    id 'com.android.application'
-    kotlin 'android'
+    id("com.android.application")
+    kotlin("android")
 }
 
 android {
-    compileSdkVersion 29
+    compileSdkVersion(29)
     defaultConfig {
-        applicationId "com.example.analogwatchface"
-        minSdkVersion 23
-        targetSdkVersion 29
-        versionCode 1
-        versionName "1.0"
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = "com.example.analogwatchface"
+        minSdkVersion(23)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            minifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 dependencies {
-    implementation 'androidx.constraintlayout:constraintlayout:1.1.3'
-    implementation 'androidx.wear:wear:1.0.0'
-    implementation 'androidx.wear:wear-watchface:1.0.0'
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.wear:wear:1.0.0")
+    implementation("androidx.wear:wear-watchface:1.0.0")
 }
